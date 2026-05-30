4c0d077911c2c68cfd4d90d9be9f55572dffc815
PIYUSH KUMAR YADAV <piyush.kr.yadav521@gmail.com>
2026-05-31 00:48:51 +0530

    Add files via upload

---
+++ tb_half_adder.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+module tb_half_adder;
+
+    reg a,b;
+    wire sum, carry;
+    half_adder uut(
+        .a(a),
+        .b(b),
+        .sum(sum),
+        .carry(carry)
+    );
+
+    initial 
+        begin 
+            $dumpfile("half_adder.vcd");
+            $dumpvars(0, tb_half_adder);
+            a=0; b=0; #10;
+            a=0; b=1; #10;
+            a=1; b=0; #10;
+            a=1; b=1; #10;
+            $finish;
+        end
+endmodule
+
+
